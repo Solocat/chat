ef02b64cde684dd0e7a24bb94232de01b1ebc478
Solocat <56864157+Solocat@users.noreply.github.com>
2020-04-01 16:34:43 +0300

    user stuff test

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var app = new Vue({
         currentMSG: "",
         messages: [],
         msgUrl: "messages.json",
-        author: uid,
+        author: "",
         database: {}
     },
     methods: {
@@ -36,6 +36,25 @@ var app = new Vue({
         getDatabase() {
             return firebase.database();
         },
+        authenticate(provider)
+        {
+            return firebase.auth().signInWithPopup(provider).then(function(result) {
+                // This gives you a Google Access Token. You can use it to access the Google API.
+                var token = result.credential.accessToken;
+                // The signed-in user info.
+                return result.user.uid;
+                // ...
+            }).catch(function(error) {
+                // Handle Errors here.
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                // The email of the user's account used.
+                var email = error.email;
+                // The firebase.auth.AuthCredential type that was used.
+                var credential = error.credential;
+                // ...
+            });
+        },
         autoScroll() {
             var objDiv = document.getElementById("messages");
             objDiv.scrollTop = objDiv.scrollHeight;
@@ -47,6 +66,12 @@ var app = new Vue({
     async mounted() {
         this.database = firebase.database();
 
+
+        var provider = new firebase.auth.GoogleAuthProvider();
+        firebase.auth().useDeviceLanguage();
+        this.author = await this.authenticate(provider);
+        console.log(this.author);
+        debugger;
         //this.messages = await this.getMessages();
 
         const vm = this;
